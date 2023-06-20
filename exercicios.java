73dce80e5fbfd56403a0e08b85f3565dcf47c330
SergioArthur115@gmail.com <sergioarthur115@gmail.com>
2023-06-20 10:05:31 -0300

    first commit

---
+++ exercicios.java
@@ -0,0 +1,8 @@
+import javax.sound.sampled.SourceDataLine;
+
+public class exercicios {
+    public static void main(final String[] args)
+    {
+        System.out.println("Hello, World!");
+    }
+}
