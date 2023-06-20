9ec244f286ef2cabffc47174c0e268f89d318cb3
SergioArthur115 <sergioarthur115@gmail.com>
2023-06-20 14:18:20 -0300

    adicionado exercicios da aula 1 e 2

--- exercicios.java
+++ exercicios.java
@@ -1,8 +1,106 @@
-import javax.sound.sampled.SourceDataLine;
+import java.util.Scanner;
 
 public class exercicios {
-    public static void main(final String[] args)
-    {
-        System.out.println("Hello, World!");
+    public static void main(final String[] args){
+        Scanner leia = new Scanner(System.in);
+        int op = 99;
+        System.out.println("|Menu|");
+        System.out.println("Digite 1 para ver o primeiro programa:");
+        System.out.println("Digite 2 para ver o programa de dados pessoais:");
+        System.out.println("Digite 3 para ver o programa de dados pessoais completos:");
+        System.out.println("Digite 0 para sair: ");
+        op = leia.nextInt();
+        switch (op) {
+            case 1:
+                primeiroPrograma();
+                break;
+
+            case 2:
+                dadosPessoais();
+                break;
+            case 3:
+                dadosPessoaisCompletos();
+                break;
+            case 0:
+                System.out.println("Fim do Programa!");
+                break;
+            default:
+                System.out.println("Entrada Inválida!!!");
+                break;
+        }
+    }
+    public static void primeiroPrograma() {
+        // primeiro programa em java
+        // Disciplina : [Linguagem e Lógica de Programação]
+        // Professor : Miguel Matiolla
+        // Descrição : Primeiro programa utilizando o Portugol
+        // Autor(a) : eu mesmo
+        // Data atual : 19/06/2023
+
+        // Inicio do nosso programa para imprimir textos na tela
+        // Comando System.out.println - ele escreve na tela e já quebra a linha
+
+        System.out.println("Hello World!!!");
+        System.out.println("Meu primeiro programa em java");
+        System.out.println("Que emoção kkkkk!!!");
+
+        // Comando System.out.print - ele escreve na tela mas não quebra a linha
+        System.out.print("Hello World!!!");
+        System.out.print("Meu primeiro programa em java!!!");
+        System.out.print("Que emoção kkkkk!!!");
+
+        System.out.print("\nMeu nome é ");
+        System.out.print("Sérgio");
+    }
+
+    public static void dadosPessoais() {
+        // Programa para realizar um cadastro básico
+        Scanner leia = new Scanner(System.in);
+        String nome, sobrenome;
+        int idade;
+        System.out.println("|Sistema de cadastro básico|");
+        System.out.println("Informe seu nome: ");
+        nome = leia.nextLine();
+        System.out.println("Informe seu sobrenome: ");
+        sobrenome = leia.nextLine();
+        System.out.println("Informe sua idade: ");
+        idade = leia.nextInt();
+        System.out.println("-----------------------------");
+        System.out.println("Seu nome é: " + nome);
+        System.out.println("Seu sobrenome é: " + sobrenome);
+        System.out.println("Sua idade é: " + idade);
+    }
+
+    public static void dadosPessoaisCompletos() {
+        // Programa para realizar um cadastro básico
+        Scanner leia = new Scanner(System.in);
+        String nome, sobrenome, cpf, sexo, rg, endereco, cidade;
+        int idade;
+        System.out.println("|Sistema de cadastro básico|");
+        System.out.println("Informe seu nome: ");
+        nome = leia.nextLine();
+        System.out.println("Informe seu sobrenome: ");
+        sobrenome = leia.nextLine();
+        System.out.println("Informe seu sexo: ");
+        sexo = leia.nextLine();
+        System.out.println("Informe seu cpf: ");
+        cpf = leia.nextLine();
+        System.out.println("Informe seu rg: ");
+        rg = leia.nextLine();
+        System.out.println("Informe seu endereco: ");
+        endereco = leia.nextLine();
+        System.out.println("Informe seu cidade: ");
+        cidade = leia.nextLine();
+        System.out.println("Informe sua idade: ");
+        idade = leia.nextInt();
+        System.out.println("-----------------------------");
+        System.out.println("Seu nome é: " + nome);
+        System.out.println("Seu sobrenome é: " + sobrenome);
+        System.out.println("Sua idade é: " + idade);
+        System.out.println("Seu sexo é: " + sexo);
+        System.out.println("Seu cpf é: " + cpf);
+        System.out.println("Seu rg é: " + rg);
+        System.out.println("Seu endereco é: " + endereco);
+        System.out.println("Sua cidade é: " + cidade);
     }
 }
